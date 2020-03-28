ab3e3a62925e68c1b42f84f8ec95f1a1bcfd0389
karen <gallegokaren1897@gmail.com>
2020-03-28 01:40:14 -0400

    add loading
    - suspense is still unstable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {lazy} from "react";
+import React, {lazy, Suspense} from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const CoronaWalrus = lazy(() => import("./CoronaWalrus"));
@@ -6,24 +6,26 @@ const Home = lazy(() => import("./Home"));
 
 const App: React.FC = () => (
   <Router>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/corona">Corona</Link>
-        </li>
-      </ul>
-    </nav>
-    <Switch>
-      <Route path="/corona">
-        <CoronaWalrus />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/corona">Corona</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/corona">
+          <CoronaWalrus />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Suspense>
   </Router>
 );
 
