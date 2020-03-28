1ebd1efdf3664d6a361529499c9dcc42e3980509
karen <gallegokaren1897@gmail.com>
2020-03-28 02:18:50 -0400

    remove home screen
    - focus render on one mini game

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import React, {lazy, Suspense} from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 const CoronaWalrus = lazy(() => import("./components/CoronaWalrus"));
-const Home = lazy(() => import("./screens/Home"));
 
 const App: React.FC = () => (
-  <Router>
+  <div>
     <Suspense fallback={<div>Loading...</div>}>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/corona">Corona</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/corona">
-          <CoronaWalrus />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <CoronaWalrus/>
     </Suspense>
-  </Router>
+  </div>
 );
 
 export default App;
--- src/screens/Home.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home: React.FC = () => (
-  <h1>Home page deploy test</h1>
-);
-
-export default Home;
