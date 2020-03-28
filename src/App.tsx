0ff2ad8b92756aedfe3d3c80f8f7483539fe529c
karen <gallegokaren1897@gmail.com>
2020-03-28 01:15:12 -0400

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import CoronaWalrus from "./CoronaWalrus";
+import Home from "./Home";
+
+const App: React.FC = () => (
+  <Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/corona">Corona</Link>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      <Route path="/corona">
+        <CoronaWalrus />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
---
+++ src/CoronaWalrus.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CoronaWalrus: React.FC = () => (
+  <h1>Corona walrus mini game</h1>
+);
+
+export default CoronaWalrus;
