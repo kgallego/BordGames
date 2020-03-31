8471c51d40143f9723499dea5abfc4aa094c692b
karen <gallegokaren1897@gmail.com>
2020-03-31 12:56:00 -0400

    add container styles on app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React, {lazy, Suspense} from "react";
 const Hover = lazy(() => import("./games/Hover/Screen"));
 
+const styles: Styles = {
+  container: {
+    margin: '5rem',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+};
+
 const App: React.FC = () => (
-  <div>
+  <div style={styles.container}>
     <Suspense fallback={<div>Loading...</div>}>
       <Hover/>
     </Suspense>
