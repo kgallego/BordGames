27da2ba99e7e12c0de1c4908518cdc53937cf856
karen <gallegokaren1897@gmail.com>
2020-03-31 21:42:05 -0400

    skip unused test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders corona walrus link', () => {
+xtest('renders corona walrus link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/corona/i);
   expect(linkElement).toBeInTheDocument();
